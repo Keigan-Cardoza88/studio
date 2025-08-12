353d65aa48c8de67b28659776ae80bb736e2be14
Keigan_Merv Cardoza <cardoza.keigs@gmail.com>
2025-08-12 13:50:05 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/setlist-sidebar.tsx
+++ src/components/setlist-sidebar.tsx
@@ -7,7 +7,7 @@ import { Sidebar, SidebarHeader, SidebarContent, SidebarMenu, SidebarMenuItem, S
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogFooter, DialogTrigger, DialogClose } from '@/components/ui/dialog';
-import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogHeader, AlertDialogTitle, AlertDialogTrigger } from '@/components/ui/alert-dialog';
+import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle, AlertDialogTrigger } from '@/components/ui/alert-dialog';
 import { Music, PlusCircle, Trash2 } from 'lucide-react';
 import { useForm, SubmitHandler } from "react-hook-form";
 
@@ -102,3 +102,4 @@ export function SetlistSidebar() {
     </Sidebar>
   );
 }
+
