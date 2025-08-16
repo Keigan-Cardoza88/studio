62820aa53fd9f6fe4a2eb965cecd54937121bfa6
Keigan_Merv Cardoza <cardoza.keigs@gmail.com>
2025-08-16 07:55:47 +0000

    undo this capacitor thing, i realised i was regenerating keystores for e

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -7,15 +7,7 @@ const config: CapacitorConfig = {
   webDir: 'out',
   server: {
     androidScheme: 'https'
-  },
-  android: {
-    buildOptions: {
-      keystorePath: undefined,
-      keystoreAlias: undefined,
-    },
-    versionName: '1.0.1', // A user-visible version name
-    versionCode: 2,      // An incremental version number for updates
-  },
+  }
 };
 
 export default config;
